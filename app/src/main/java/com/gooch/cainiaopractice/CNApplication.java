6f5767ff8d1d7cad4a66610dc762157c3f7cb9df
zhaoguangchao <careerproject@163.com>
2017-08-08 23:37:37 +0800

    edit

---
+++ app/src/main/java/com/gooch/cainiaopractice/CNApplication.java
@@ -0,0 +1,23 @@
+package com.gooch.cainiaopractice;
+
+import android.app.Application;
+
+/**
+ * <pre>
+ * author : gooch
+ * e-mail : zhaoguangchao@100tal.com
+ * time   : 2017/8/8
+ * desc   :
+ * version: 1.0
+ * </pre>
+ */
+
+
+public class CNApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+  }
+}
